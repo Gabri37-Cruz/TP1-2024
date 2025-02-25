8b309b1e298e07aa8c25cd9d47262c25cca67467
Gabri37-Cruz <gabriel.pinheiro@gsuite.iff.edu.br>
2025-02-25 15:11:37 -0300

    Add files via upload

--- script25.02.js
+++ script25.02.js
@@ -6,5 +6,33 @@ const lista = document.getElementById("lista-cadastros")
 let cadastros = []
 
 if (localStorage.getItem("cadastros")){
-    cadastros = JSON.parse(localStorage.getItem())
+    cadastros = JSON.parse(localStorage.getItem("cadastros"))
 }
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault()
+    const nome = document.getElementById("nome").value
+    const email = document.getElementById("email").value
+    const telefone = document.getElementById("telefone").value
+    const cadastro = {
+        nome,
+        email,
+        telefone
+    }
+    cadastros.push(cadastro)
+    localStorage.setItem("cadastros", JSON.stringify
+    (cadastros))
+    form.reset()
+}
+)
+
+botaoi.addEventListener("click", ()=> {
+    lista.innerHTML = ""
+    cadastros.forEach((cadastro, indice)=>{
+        const item = document.createElement('p')
+        item.textContent = `Cadastro ${indice+1}: ${cadastro.nome} - ${cadastro.email} - ${cadastro.telefone}`
+        lista.appendChild(item)
+    }
+    ) 
+}
+)
